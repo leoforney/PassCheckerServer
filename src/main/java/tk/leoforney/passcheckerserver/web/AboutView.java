177dd60d4c30483f09cf915758e34ba28ef1812d
Leo Forney <forneyleo@gmail.com>
2019-01-19 14:34:15 -0600

    Add Jquery QRCode but not executing javascript

--- src/main/java/tk/leoforney/passcheckerserver/web/AboutView.java
+++ src/main/java/tk/leoforney/passcheckerserver/web/AboutView.java
@@ -1,5 +1,7 @@
 package tk.leoforney.passcheckerserver.web;
 
+import com.vaadin.flow.component.Html;
+import com.vaadin.flow.component.dependency.JavaScript;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -17,11 +19,15 @@ import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.*;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import static tk.leoforney.passcheckerserver.UserManagement.authenticated;
 import static tk.leoforney.passcheckerserver.web.AppView.setTitle;
 
 @Route("about")
+@JavaScript("bower_components/jquery/dist/jquery.min.js")
+@JavaScript("bower_components/jquery-qrcode/jquery.qrcode.min.js")
 public class AboutView extends VerticalLayout {
 
     private static Logger logger = LoggerFactory.getLogger(AboutView.class);
@@ -73,10 +79,22 @@ public class AboutView extends VerticalLayout {
         //QRCode code = null;
         if (tokenObject != null && authenticated(String.valueOf(tokenObject))) {
             String[] hash = Arrays.toString(base64.decode(String.valueOf(tokenObject))).split(":");
-            String email = hash[0];
-            String password = hash[1];
-            String ip = ips.get(0);
+            //String email = hash[0];
+            //String password = hash[1];
+            //String ip = ips.get(0);
             //code = new QRCode(Base64.encodeBase64String((email + ":" + password + ":" + ip).getBytes()));
+            Html html = new Html("<div id=\"qrcode\"></div>");
+            add(html);
+            final ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
+            executor.schedule(() -> {
+                if (getUI().isPresent()) {
+                    System.out.println("UI Present");
+                    // TODO: Finish QR code
+                    getUI().get().getPage().executeJavaScript(
+                            "jQuery('#qrcode').qrcode({width: 124,height: 124,text: \"size doesn't matter\"});");
+                }
+            }, 5, TimeUnit.SECONDS);
+
         }
 
         //add(QRCode);
