a63a8627d7de90990e6070439d5e3b6ef73f460a
Leo Forney <forneyleo@gmail.com>
2019-01-22 20:36:56 -0600

    Add bower components for missing dependencies, not use WebJars

--- src/main/java/tk/leoforney/passcheckerserver/web/AboutView.java
+++ src/main/java/tk/leoforney/passcheckerserver/web/AboutView.java
@@ -1,7 +1,5 @@
 package tk.leoforney.passcheckerserver.web;
 
-import com.vaadin.flow.component.Html;
-import com.vaadin.flow.component.dependency.JavaScript;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -19,15 +17,11 @@ import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.util.*;
-import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 import static tk.leoforney.passcheckerserver.UserManagement.authenticated;
 import static tk.leoforney.passcheckerserver.web.AppView.setTitle;
 
 @Route("about")
-@JavaScript("bower_components/jquery/dist/jquery.min.js")
-@JavaScript("bower_components/jquery-qrcode/jquery.qrcode.min.js")
 public class AboutView extends VerticalLayout {
 
     private static Logger logger = LoggerFactory.getLogger(AboutView.class);
@@ -40,7 +34,7 @@ public class AboutView extends VerticalLayout {
 
         boolean idea = false;
         if (Main.arguments.length > 0) {
-            for (String arg: Main.arguments) {
+            for (String arg : Main.arguments) {
                 if (arg.equalsIgnoreCase("idea")) idea = true;
             }
         }
@@ -70,34 +64,21 @@ public class AboutView extends VerticalLayout {
         add(new Label("Java Version: " + System.getProperty("java.version")));
         add(new Label("Vaadin Version: " + prop.get("vaadin")));
         List<String> ips = ipAddress();
-        for (String string: ips) {
+        for (String string : ips) {
             add(new Label("IP: " + string));
         }
         add(new Label("Made by BDSL: Bria R, Dana L., Santiago C., Leo"));
         Object tokenObject = VaadinSession.getCurrent().getAttribute("Token");
 
-        //QRCode code = null;
+        GraniteQRCode qrCode = null;
         if (tokenObject != null && authenticated(String.valueOf(tokenObject))) {
             String[] hash = Arrays.toString(base64.decode(String.valueOf(tokenObject))).split(":");
-            //String email = hash[0];
-            //String password = hash[1];
-            //String ip = ips.get(0);
-            //code = new QRCode(Base64.encodeBase64String((email + ":" + password + ":" + ip).getBytes()));
-            Html html = new Html("<div id=\"qrcode\"></div>");
-            add(html);
-            final ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
-            executor.schedule(() -> {
-                if (getUI().isPresent()) {
-                    System.out.println("UI Present");
-                    // TODO: Finish QR code
-                    getUI().get().getPage().executeJavaScript(
-                            "jQuery('#qrcode').qrcode({width: 124,height: 124,text: \"size doesn't matter\"});");
-                }
-            }, 5, TimeUnit.SECONDS);
-
+            String email = hash[0];
+            String password = "";
+            String ip = ips.get(0);
+            qrCode = new GraniteQRCode(email);
+            add(qrCode);
         }
-
-        //add(QRCode);
 
     }
 
@@ -105,7 +86,7 @@ public class AboutView extends VerticalLayout {
         List<String> ips = new ArrayList<>();
         try {
             Enumeration e = NetworkInterface.getNetworkInterfaces();
-            while(e.hasMoreElements()) {
+            while (e.hasMoreElements()) {
                 NetworkInterface n = (NetworkInterface) e.nextElement();
                 Enumeration ee = n.getInetAddresses();
                 while (ee.hasMoreElements()) {
---
+++ src/main/java/tk/leoforney/passcheckerserver/web/GraniteQRCode.java
@@ -0,0 +1,62 @@
+package tk.leoforney.passcheckerserver.web;
+
+import com.vaadin.flow.component.Tag;
+import com.vaadin.flow.component.dependency.HtmlImport;
+import com.vaadin.flow.component.html.Div;
+import org.springframework.lang.NonNull;
+
+@HtmlImport("bower_components/granite-qrcode-generator/granite-qrcode-generator.html")
+@Tag("granite-qrcode-generator")
+public class GraniteQRCode extends Div {
+
+    public GraniteQRCode() {
+        setMode(Mode.ALPHANUMERIC);
+        setData("");
+        generateQRCode();
+    }
+
+    public GraniteQRCode(String data) {
+        setMode(Mode.ALPHANUMERIC);
+        setData(data);
+        generateQRCode();
+    }
+
+    public void setMode(@NonNull Mode mode) {
+        System.out.println(mode.toString());
+        getElement().setProperty("mode", mode.toString().toLowerCase());
+        paramsChanged();
+    }
+
+    public void setData(String value) {
+        getElement().setProperty("data", value);
+        paramsChanged();
+    }
+
+    public String getData() {
+        return getElement().getProperty("data");
+    }
+
+    public void setAuto(boolean auto) {
+        getElement().setProperty("auto", auto);
+        paramsChanged();
+    }
+
+    public boolean getAuto() {
+        return Boolean.parseBoolean(getElement().getProperty("auto"));
+    }
+
+    public void generateQRCode() {
+        getElement().callFunction("generateQRCode");
+    }
+
+    public void paramsChanged() {
+        getElement().callFunction("paramsChanged");
+    }
+
+    public enum Mode {
+        NUMERIC,
+        ALPHANUMERIC,
+        OCTET
+    }
+
+}
